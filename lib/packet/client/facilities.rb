fff7bb5a8438368c81e5f359e3f438c639123567
Jayson Camba <mjmcamba@gmail.com>
2019-01-13 13:27:28 +0800

    list projects facilities

--- lib/packet/client/facilities.rb
+++ lib/packet/client/facilities.rb
@@ -4,6 +4,10 @@ module Packet
       def list_facilities(*args)
         get('facilities', *args).body['facilities'].map { |p| Packet::Facility.new(p, self) }
       end
+
+      def list_project_facilities(project_id, *args)
+        get("projects/#{project_id}/facilities", *args).body['facilities'].map { |p| Packet::Facility.new(p, self) }
+      end
     end
   end
 end
