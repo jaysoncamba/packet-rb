ff05c73c3a30d07784e93e5f9a283a984c708aff
Jayson Camba <mjmcamba@gmail.com>
2019-01-13 10:46:24 +0800

    list plans by project

--- lib/packet/client/plans.rb
+++ lib/packet/client/plans.rb
@@ -4,6 +4,10 @@ module Packet
       def list_plans(*args)
         get('plans', *args).body['plans'].map { |p| Packet::Plan.new(p, self) }
       end
+
+      def list_project_plans(project_id, *args)
+        get("plans/#{project_id}/devices", *args).body['plans'].map { |p| Packet::Plan.new(p, self) }
+      end
     end
   end
 end
